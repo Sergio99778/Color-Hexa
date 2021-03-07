649d677ff5175561fd7adb4fac4ba195627c68ab
sergio99778 <sergio4brandon@gmail.com>
2021-03-07 11:35:20 -0600

    Adding background div.

--- js/color.js
+++ js/color.js
@@ -3,4 +3,6 @@ const salida = document.querySelector("#salida");
 color.addEventListener("input", () => {
     const value = color.value;
     salida.innerHTML = value;
-})
+    salida.style.background = value;
+    salida.style.color = "#fff";
+});
